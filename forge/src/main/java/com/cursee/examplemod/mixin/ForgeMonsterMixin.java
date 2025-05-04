ac3d6ee5a838bdbddd8f3be94945f49e73b66f77
jason13official <jason13official@gmail.com>
2025-05-03 23:02:10 -0400

    test one?

---
+++ forge/src/main/java/com/cursee/examplemod/mixin/ForgeMonsterMixin.java
@@ -0,0 +1,30 @@
+package com.cursee.examplemod.mixin;
+
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.ServerLevelAccessor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Monster.class)
+public class ForgeMonsterMixin {
+
+    @Redirect(method = "checkMonsterSpawnRules", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/ServerLevelAccessor;getDifficulty()Lnet/minecraft/world/Difficulty;"))
+    private static Difficulty peaceful_monsters$checkMonsterSpawnRulesRedirect(ServerLevelAccessor instance) {
+        return Difficulty.EASY;
+    }
+
+    @Redirect(method = "checkAnyLightMonsterSpawnRules", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/LevelAccessor;getDifficulty()Lnet/minecraft/world/Difficulty;"))
+    private static Difficulty peaceful_monsters$checkAnyLightMonsterSpawnRulesRedirect(LevelAccessor instance) {
+        return Difficulty.EASY;
+    }
+
+    @Inject(method = "shouldDespawnInPeaceful", at = @At(value = "TAIL"), cancellable = true)
+    private void peaceful_monsters$shouldDespawnInPeacefulInject(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(false);
+    }
+}
